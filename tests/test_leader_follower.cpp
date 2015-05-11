c9027535de3938e246255cea1884cc5913a91b6d
Ricardo <makiolo@gmail.com>
2015-05-11 19:16:37 +0200

    Update test_leader_follower.cpp

--- tests/test_leader_follower.cpp
+++ tests/test_leader_follower.cpp
@@ -32,11 +32,8 @@ public:
 		std::cout << std::endl;
 	}
 
-	sas::syncronizer& talk() { return _talking; }
-
 protected:
 	std::mutex _lock;
-	sas::syncronizer _talking;
 };
 
 class PersonA;
@@ -88,7 +85,8 @@ int main()
 	std::ios_base::sync_with_stdio(false);
 	
 	{
-		auto pool = std::make_shared<fes::thread_pool>(std::thread::hardware_concurrency());
+		//auto pool = std::make_shared<fes::thread_pool>(std::thread::hardware_concurrency());
+		auto pool = std::make_shared<fes::thread_pool>(1);
 		
 		Context context;
 		PersonA person1("Person A", context);
@@ -109,52 +107,39 @@ int main()
 		 Person A: "7. Bye person B"
 		 Person B: "8. Bye person A"
 		*/
+		
 		// person A
 		person1.call_me([&](PersonA& self) {
 			self.say("1. What are you doing now ? ");
 			self.sleep(10);
-			context.talk().signal();
 		}, 0, 4);
 		person1.call_me([&](PersonA& self) {
-			context.talk().wait();
 			self.say("3. I didn't know you play pool.  Are you having fun?");
 			self.sleep(10);
-			context.talk().signal();
 		}, 0, 3);
 		person1.call_me([&](PersonA& self) {
-			context.talk().wait();
 			self.say("5. I'm taking a break from my homework. There seems to be no end to the amount of work I have to do.");
 			self.sleep(10);
-			context.talk().signal();
 		}, 0, 2);
 		person1.call_me([&](PersonA& self) {
-			context.talk().wait();
 			self.say("7. bye person B");
 			self.sleep(10);
-			context.talk().signal();
 		}, 0, 1);
-
+		
 		// person B
 		person2.call_me([&](PersonB& self) {
-			context.talk().wait();
 			self.say("2. I'm playing pool with my friends at a pool hall.");
 			self.sleep(10);
-			context.talk().signal();
 		}, 0, 4);
 		person2.call_me([&](PersonB& self) {
-			context.talk().wait();
 			self.say("4. I'm having a great time.  How about you?  What are you doing?");
 			self.sleep(10);
-			context.talk().signal();
 		}, 0, 3);
 		person2.call_me([&](PersonB& self) {
-			context.talk().wait();
 			self.say("6. I'm glad I'm not in your shoes.");
 			self.sleep(10);
-			context.talk().signal();
 		}, 0, 2);
 		person2.call_me([&](PersonB& self) {
-			context.talk().wait();
 			self.say("8. bye person A");
 			self.sleep(10);
 		}, 0, 1);
