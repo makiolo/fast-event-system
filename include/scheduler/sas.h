abaa2b1a32d8e1156e4d34203a59ca71ec9e0178
Ricardo Marmolejo García <makiolo@gmail.com>
2015-05-15 21:30:07 +0200

    move sas code to cpp

--- include/scheduler/sas.h
+++ include/scheduler/sas.h
@@ -19,7 +19,7 @@
 
 namespace sas {
 
-class processor
+class fast_event_system_API processor
 {
 public:
 	processor(size_t);
@@ -40,63 +40,6 @@ private:
 	std::condition_variable _condition;
 	bool _stop;
 };
- 
-inline processor::processor(size_t threads) : _stop(false)
-{
-	for (size_t i = 0; i < threads; ++i)
-	{
-		_workers.emplace_back(
-			[this]
-			{
-				for (;;)
-				{
-					std::shared_ptr< std::packaged_task<void()> > task;
-					
-					{
-						std::unique_lock<std::mutex> lock(this->_queue_mutex);
-						this->_condition.wait(lock, [this]{ return this->_stop || !this->_tasks.empty(); });
-						if (this->_stop && this->_tasks.empty())
-						{
-							return;
-						}
-						task = std::move(this->_tasks.front());
-						this->_tasks.pop();
-					}
-					
-					(*task)();
-				}
-			}
-		);
-	}
-}
-
-void processor::enqueue(const std::shared_ptr< std::packaged_task<void()> >& func)
-{
-	{
-		std::unique_lock<std::mutex> lock(_queue_mutex);
-		
-		if(!_stop)
-		{
-			_tasks.emplace(func);
-		}
-	}
-	_condition.notify_one();
-}
-
-// the destructor joins all threads
-inline processor::~processor()
-{
-	{
-		std::unique_lock<std::mutex> lock(_queue_mutex);
-		_stop = true;
-	}
-	_condition.notify_all();
-	
-	for (auto &worker : _workers)
-	{
-		worker.join();
-	}
-}
 
 template <typename T>
 class commands_queue
