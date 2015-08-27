36504162a26b86c43e35c4269544035fbc13e8d7
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2015-08-27 23:51:44 +0200

    new test

---
+++ tests/test_async_fast_autoconnect.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fast-event-system/fes.h>
+
+int main(int, const char**)
+{
+	const int N = 5;
+	int counter = 0;
+
+	fes::async_fast<std::string> sync;
+
+	// testing autoconnect
+	sync.connect(sync);
+		
+	fes::connection<std::string> conn(
+		sync.connect(
+			[&counter](const std::string& str)
+			{
+				std::cout << str << std::endl;
+				++counter;
+			}
+		)
+	);
+	
+	sync("autoreference");
+	for (int i = 0; i < N; ++i) {
+		sync.dispatch_one();
+	}
+
+	return counter == N;
+}
+
