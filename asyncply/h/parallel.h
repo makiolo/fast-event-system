369c2e87d2d2dd5e7b74c9cef5c1a2f5136fadd5
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2015-09-26 03:03:15 +0200

    increase functionality in parallel

--- asyncply/h/parallel.h
+++ asyncply/h/parallel.h
@@ -32,6 +32,46 @@ void parallel(std::vector<shared_task<Function>>& vf, Function&& f)
 	vf.emplace_back(asyncply::run(std::forward<Function>(f)));
 }
 
+template <  typename Function,
+			typename... Functions>
+typename std::result_of<Function()>::type parallel(Function&& f, Functions&&... fs)
+{
+	using ret_t = typename std::result_of<Function()>::type;
+	std::vector<shared_task<Function>> vf;
+	vf.emplace_back(asyncply::run(std::forward<Function>(f)));
+	asyncply::_parallel(vf, std::forward<Functions>(fs)...);
+	std::vector<ret_t> results;
+	for(auto& v : vf)
+		results.emplace_back(v->get());
+	return std::accumulate(results.begin(), results.end(), ret_t(0), std::plus<ret_t>());
+}
+
+template <  typename Function,
+			typename... Functions,
+			typename = std::enable_if_t<std::is_same<typename std::result_of<Function()>::type, bool>::value>
+	>
+bool parallel(Function&& f, Functions&&... fs)
+{
+	std::vector<shared_task<Function>> vf;
+	vf.emplace_back(asyncply::run(std::forward<Function>(f)));
+	asyncply::_parallel(vf, std::forward<Functions>(fs)...);
+	std::vector<bool> results;
+	for(auto& v : vf)
+		results.emplace_back(v->get());
+	return std::accumulate(results.begin(), results.end(), true, std::logical_and<bool>());
+}
+
+template <  typename Function,
+			typename... Functions,
+			typename = std::enable_if_t<std::is_void<typename std::result_of<Function()>::type>::value>
+	>
+void parallel(Function&& f, Functions&&... fs)
+{
+	std::vector<shared_task<Function>> vf;
+	vf.emplace_back(asyncply::run(std::forward<Function>(f)));
+	asyncply::_parallel(vf, std::forward<Functions>(fs)...);
+}
+
 }
 
 #endif
