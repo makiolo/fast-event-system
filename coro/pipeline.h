1b1e36015f0195b853fbd2cb8d372121c614f294
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2015-09-30 00:15:10 +0200

    experiments with coroutines

--- coro/pipeline.h
+++ coro/pipeline.h
@@ -61,5 +61,105 @@ public:
 
 // }
 
+namespace asyncply {
+
+template <typename YIELD_TYPE>
+using coro = boost::coroutines::coroutine<YIELD_TYPE()>;
+
+template <typename YIELD_TYPE>
+using yield_type = typename coro<YIELD_TYPE>::caller_type;
+
+template <typename YIELD_TYPE, typename Function>
+std::shared_ptr< coro<YIELD_TYPE> > corun(Function&& f)
+{
+	return std::make_shared<coro<YIELD_TYPE> >(std::forward<Function>(f));
+}
+
+// template <typename Function>
+// void _pipeline(std::vector<shared_task<Function>>& vf, Function&& f)
+// {
+// 	vf.emplace_back(asyncply::run(std::forward<Function>(f)));
+// }
+//
+// template <typename Function, typename... Functions>
+// void _pipeline(std::vector<shared_task<Function>>& vf, Function&& f, Functions&&... fs)
+// {
+// 	vf.emplace_back(asyncply::run(std::forward<Function>(f)));
+// 	asyncply::_pipeline(vf, std::forward<Functions>(fs)...);
+// }
+
+// template <  typename Function,
+// 			typename... Functions,
+// 			class = typename std::enable_if<
+// 				(!std::is_arithmetic<typename std::result_of<Function()>::type>::value) &&
+// 				(!std::is_same<typename std::result_of<Function()>::type, bool>::value) &&
+// 				(!std::is_void<typename std::result_of<Function()>::type>::value)
+// 			>::type
+// 	>
+// std::vector<typename std::result_of<Function()>::type> pipeline(Function&& f, Functions&&... fs)
+// {
+// 	using ret_t = typename std::result_of<Function()>::type;
+// 	std::vector<shared_task<Function>> vf;
+// 	vf.emplace_back(asyncply::run(std::forward<Function>(f)));
+// 	asyncply::_pipeline(vf, std::forward<Functions>(fs)...);
+// 	std::vector<ret_t> results;
+// 	for(auto& v : vf)
+// 		results.emplace_back(v->get());
+// 	return std::move(results);
+// }
+//
+// template <  typename Function,
+// 			typename... Functions,
+// 			class = typename std::enable_if<
+// 				(std::is_arithmetic<typename std::result_of<Function()>::type>::value) &&
+// 				(!std::is_same<typename std::result_of<Function()>::type, bool>::value)
+// 			>::type
+// 	>
+// typename std::result_of<Function()>::type pipeline(Function&& f, Functions&&... fs)
+// {
+// 	using ret_t = typename std::result_of<Function()>::type;
+// 	std::vector<shared_task<Function>> vf;
+// 	vf.emplace_back(asyncply::run(std::forward<Function>(f)));
+// 	asyncply::_pipeline(vf, std::forward<Functions>(fs)...);
+// 	std::vector<ret_t> results;
+// 	for(auto& v : vf)
+// 		results.emplace_back(v->get());
+// 	return std::accumulate(results.begin(), results.end(), ret_t(0), std::plus<ret_t>());
+// }
+//
+// template <  typename Function,
+// 			typename... Functions,
+// 			class = typename std::enable_if<
+// 				(std::is_same<typename std::result_of<Function()>::type, bool>::value)
+// 			>::type
+// 	>
+// bool pipeline(Function&& f, Functions&&... fs)
+// {
+// 	std::vector<shared_task<Function>> vf;
+// 	vf.emplace_back(asyncply::run(std::forward<Function>(f)));
+// 	asyncply::_pipeline(vf, std::forward<Functions>(fs)...);
+// 	std::vector<bool> results;
+// 	for(auto& v : vf)
+// 		results.emplace_back(v->get());
+// 	return std::accumulate(results.begin(), results.end(), true, std::logical_and<bool>());
+// }
+//
+// template <  typename Function,
+// 			typename... Functions,
+// 			class = typename std::enable_if<
+// 				(std::is_void<typename std::result_of<Function()>::type>::value)
+// 			>::type
+// 	>
+// void pipeline(Function&& f, Functions&&... fs)
+// {
+// 	std::vector<shared_task<Function> > vf;
+// 	vf.emplace_back(asyncply::run(std::forward<Function>(f)));
+// 	asyncply::_pipeline(vf, std::forward<Functions>(fs)...);
+// 	for(auto& v : vf)
+// 		v->get();
+// }
+
+}
+
 #endif
 
--- tests2/coro.cpp
+++ tests2/coro.cpp
@@ -4,6 +4,7 @@
 #include <coro/pipeline.h>
 #include <coro/cmd.h>
 #include <asyncply/h/parallel.h>
+#include <fes/h/async_fast.h>
 
 int main()
 {
@@ -32,38 +33,157 @@ int main()
 	// 	f();
 	// }
 
-	// pipelines in parallel
-	try
+	std::vector<std::shared_ptr<asyncply::coro<int> > > coros;
+	for(int i=1; i<=10; ++i)
 	{
-		auto result = asyncply::parallel(
-			[](){
-				return "one";
-			},
-			[](){
-				cmd({
-					find(".."),
-					grep(".*\\.cpp$|.*\\.h$"),
-					cat(),
-					grep("class|struct|typedef|using|void|int|double|float"),
-					grep_v("enable_if|;|\"|\'"),
-					trim(),
-					split(" "),
-					uniq(),
-					join(" "),
-					out()
-				});
-				return "two";
+		coros.emplace_back(asyncply::corun<int>(
+			[i](asyncply::yield_type<int>& yield)
+			{
+				std::cout << "create " << i << std::endl;
+				yield(0);
+				std::cout << "download " << i << std::endl;
+				yield(0);
+				std::cout << "patching " << i << std::endl;
+				yield(0);
+				std::cout << "compile " << i << std::endl;
+				yield(0);
+				std::cout << "tests " << i << std::endl;
+				yield(0);
+				std::cout << "packing " << i << std::endl;
+				yield(0);
 			}
-		);
-		std::cout << result.size() << std::endl;
-		for(auto& r : result)
+		));
+	}
+	bool pipeline = false;
+	if(!pipeline)
+	{
+		for(auto& c : coros)
 		{
-			std::cout << r << std::endl;
+			while(*c)
+			{
+				(*c)();
+			}
 		}
 	}
-	catch(boost::filesystem::filesystem_error& e)
+	else
 	{
-		std::cout << "exception: " << e.what() << std::endl;
+		bool all_finished = false;
+		while(!all_finished)
+		{
+			all_finished = true;
+			for(auto& c : coros)
+			{
+				if(*c)
+				{
+					(*c)();
+					all_finished = false;
+				}
+			}
+		}
 	}
+
+
+	fes::async_fast< std::shared_ptr<asyncply::coro<int> > > _channel;
+	std::atomic<bool> _exit;
+	_exit = false;
+	asyncply::parallel(
+			[&](){
+				while(!_exit)
+				{
+					_channel.connect([&](const std::shared_ptr<asyncply::coro<int> >& coro) {
+						while(*coro)
+						{
+							int x = coro->get();
+							_exit = (x == 1);
+							(*coro)();
+						}
+					});
+					_channel.update();
+				}
+			},
+			[&](){
+				asyncply::run(
+					[&](){
+						for(int i=0; i<1000; ++i)
+						{
+							_channel(asyncply::corun<int>(
+								[](asyncply::yield_type<int>& yield)
+								{
+									std::cout << "create " << std::endl;
+									yield(0);
+									std::cout << "download " << std::endl;
+									yield(0);
+									std::cout << "patching " << std::endl;
+									yield(0);
+									std::cout << "compile " << std::endl;
+									yield(0);
+									std::cout << "tests " << std::endl;
+									yield(0);
+									std::cout << "packing " << std::endl;
+									yield(0);
+								}
+							));
+						}
+					}
+					,
+					[&](){
+						_channel(asyncply::corun<int>(
+							[](asyncply::yield_type<int>& yield)
+							{
+								std::cout << "request exit" << std::endl;
+								yield(1);
+							}
+						));
+					}
+				);
+			},
+			[&](){
+				_channel(asyncply::corun<int>(
+					[](asyncply::yield_type<int>& yield)
+					{
+						std::cout << "step1 - thread3 " << std::endl;
+						yield(0);
+						std::cout << "step2 - thread3 " << std::endl;
+						yield(0);
+						std::cout << "step3 - thread3 " << std::endl;
+						yield(0);
+					}
+				));
+			}
+	);
+
+	// pipelines in parallel
+	// try
+	// {
+	// 	auto result = asyncply::parallel(
+	// 		[](){
+	// 			return "one";
+	// 		},
+	// 		[](){
+	// 			cmd({
+	// 				find(".."),
+	// 				grep(".*\\.cpp$|.*\\.h$"),
+	// 				cat(),
+	// 				grep("class|struct|typedef|using|void|int|double|float"),
+	// 				grep_v("enable_if|;|\"|\'"),
+	// 				trim(),
+	// 				split(" "),
+	// 				uniq(),
+	// 				join(" "),
+	// 				out()
+	// 			});
+	// 			return "two";
+	// 		}
+	// 	);
+	// 	std::cout << result.size() << std::endl;
+	// 	for(auto& r : result)
+	// 	{
+	// 		std::cout << r << std::endl;
+	// 	}
+	// }
+	// catch(boost::filesystem::filesystem_error& e)
+	// {
+	// 	std::cout << "exception: " << e.what() << std::endl;
+	// }
 }
 
