defff531948db92cbdc329325a6c098146ed5bb7
Ricardo <makiolo@gmail.com>
2016-11-15 12:51:34 +0100

    Update test_sync.cpp

--- tests/test_sync.cpp
+++ tests/test_sync.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
 #include "../sync.h"
+#include <gtest/gtest.h>
 
-int main(int, const char**)
+class SyncTest : testing::Test { };
+
+TEST(SyncTest, Test1)
 {
-	// asyncply::run( functor )
-	// asyncply::sequence( functor, functor ... )
-	// asyncply::parallel( functor, functor ... )
-	//
-	// safe vs unsafe
-	// safe vs fast
-	// inter vs intra
-	// fes::channel::safe::sync            // (future/promise)
-	// fes::channel::safe::async
-	// fes::channel::unsafe::sync
-	// fes::channel::unsafe::async         (delay)
-	//
+	// fes::safe::sync
+	// fes::safe::async
+	// fes::unsafe::sync
+	// fes::unsafe::async
 
 	fes::sync<int, std::string, double> sync;
 
 	// test connect in context
 	{
-		// TODO: operator=
 		fes::connection<int, std::string, double> conn(
 			sync.connect([](int n, const std::string& str, double r)
 				{
@@ -28,15 +22,10 @@ int main(int, const char**)
 					std::cout << "n = " << n << std::endl;
 					std::cout << "str = " << str << std::endl;
 					std::cout << "r = " << r << std::endl;
-					if (str == "kill")
-					{
-						exit(1);
-					}
+					ASSERT_STRNE(str.c_str(), "kill");
 				}));
 		// lambda must received this
 		sync(5, "hello world", 11.0);
-
-		// autodisconnection
 	}
 	// kill only if autodisconnection failed
 	sync(6, "kill", 12.0);
