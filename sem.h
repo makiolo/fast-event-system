7e049498a7710c179e162581cedb367fa49f59e2
Ricardo <makiolo@gmail.com>
2016-11-17 18:30:16 +0100

    Update sem.h

--- sem.h
+++ sem.h
@@ -3,27 +3,30 @@
 
 #include <mutex>
 #include <condition_variable>
+// #include <fast-event-system/concurrentqueue/blockingconcurrentqueue.h>
 
 namespace fes {
+
+#if 1
 
 class semaphore
 {
 public:
-    explicit semaphore(unsigned long len = 0) : _len(len) { ; }
+	explicit semaphore(unsigned long len = 0) : _len(len) { ; }
 
-    void notify()
-    {
-        std::unique_lock<std::mutex> lock(_mutex);
-        ++_len;
-        _cond.notify_one();
-    }
+	void notify()
+	{
+		std::unique_lock<std::mutex> lock(_mutex);
+		++_len;
+		_cond.notify_one();
+	}
 
-    void wait()
-    {
-        std::unique_lock<std::mutex> lock(_mutex);
-		_cond.wait(lock, [this](){return (this->size() > 0);});
-        --_len;
-    }
+	void wait()
+	{
+		std::unique_lock<std::mutex> lock(_mutex);
+			_cond.wait(lock, [this](){return (this->size() > 0);});
+		--_len;
+	}
 
 	inline unsigned long size() const
 	{
@@ -31,12 +34,17 @@ public:
 	}
 
 protected:
-    std::mutex _mutex;
-    std::condition_variable _cond;
-    unsigned long _len;
+	std::mutex _mutex;
+	std::condition_variable _cond;
+	unsigned long _len;
 };
+
+#else
+
+using semaphore = moodycamel::details::mpmc_sema::Semaphore;
+
+#endif
 
 }
 
 #endif
-
