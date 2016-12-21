96127a3c38001b05cc9d4900ef83505902a15f2e
Ricardo <makiolo@gmail.com>
2016-12-21 19:10:04 +0100

    Update common.h

--- common.h
+++ common.h
@@ -186,6 +186,24 @@ std::vector<T> tuple_to_vector(const std::tuple<T, Args...>& t)
     return v;
 }
 
+template <typename T , typename... Ts>
+auto head(const std::tuple<T,Ts...>& t)
+{
+   return std::get<0>(t);
+}
+
+template <std::size_t... Ns, typename... Ts>
+auto tail_impl(std::index_sequence<Ns...>, const std::tuple<Ts...>& t)
+{
+   return std::make_tuple(std::get<Ns+1u>(t)...);
+}
+
+template <typename... Ts>
+auto tail(const std::tuple<Ts...>& t)
+{
+   return tail_impl(std::make_index_sequence<sizeof...(Ts) - 1u>(), t);
+}
+
 } // end namespace mc
 
 // method macros
