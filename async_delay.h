fa97f68738858c82654c19dba4fb7a9538339f7a
Ricardo <makiolo@gmail.com>
2017-01-27 10:04:33 +0100

    Update async_delay.h

--- async_delay.h
+++ async_delay.h
@@ -33,8 +33,7 @@ public:
 	void operator()(int priority, deltatime delay, PARMS&&... data)
 	{
 		marktime delay_point = high_resolution_clock() + delay;
-		// _queue.emplace_back(priority, delay_point, data...);
-		_queue.push_back(message<Args...>(priority, delay_point, std::forward<PARMS>(data)...));
+		_queue.emplace_back(priority, delay_point, std::forward<PARMS>(data)...);
 		std::sort(std::begin(_queue), std::end(_queue), message_comp<Args...>());
 		_sem.notify();
 	}
