6f20f254c5412c717fb81412d6172d35548e8f39
Ricardo <makiolo@gmail.com>
2017-01-27 01:43:01 +0100

    Update test_async_fast.cpp

--- tests/test_async_fast.cpp
+++ tests/test_async_fast.cpp
@@ -28,3 +28,71 @@ TEST(AsyncFastTest, Test1)
 	sync.update();
 }
 
+struct foo
+{	
+	foo()
+		: _str("bar")
+	{
+	}
+	
+	foo(const foo& other)
+		: _str(other._str)
+	{
+		std::cout << "constructor copy foo" << std::endl;
+	}
+
+	foo(foo&& other) noexcept
+		: _str(std::move(other._str))
+	{
+		std::cout << "constructor move foo" << std::endl;
+	}
+
+	~foo() {}
+	
+	void swap(foo& other) noexcept
+	{
+		std::cout << "swap foo" << std::endl;
+		using std::swap;
+		std::swap(_str, other._str);
+	}
+
+	foo& operator=(const foo& other)
+	{
+		std::cout << "operator copy foo" << std::endl;
+		foo(other).swap(*this);
+		return *this;
+	}
+
+	foo& operator=(foo&& other) noexcept
+	{
+		std::cout << "operator move foo" << std::endl;
+		foo(std::move(other)).swap(*this);
+		return *this;
+	}
+public:
+	std::string _str;
+};
+
+TEST(AsyncFastTest, Test2)
+{
+	fes::async_fast<foo> sync;
+	sync.connect([](auto&& f)
+		{
+			std::cout << "<async_fast> received rvalue" << std::endl;
+		});
+	sync(foo());
+	sync.update();
+}
+
+TEST(AsyncFastTest, Test3)
+{
+	fes::async_fast<foo> sync;
+	sync.connect([](auto&& f)
+		{
+			std::cout << "<async_fast> received lvalue" << std::endl;
+		});
+	foo f;
+	sync(f);
+	sync.update();
+}
+
