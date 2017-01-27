528e6632a4ba87addfbffff2f8f70ff1dd9ea881
Ricardo <makiolo@gmail.com>
2017-01-27 01:43:55 +0100

    Update test_async_delay.cpp

--- tests/test_async_delay.cpp
+++ tests/test_async_delay.cpp
@@ -35,3 +35,73 @@ TEST(AsyncDelayTest, Test1)
 
 	ASSERT_TRUE(is_dispatched);
 }
+
+
+struct foo
+{	
+	foo()
+		: _str("bar")
+	{
+	}
+	
+	foo(const foo& other)
+		: _str(other._str)
+	{
+		std::cout << "constructor copy foo" << std::endl;
+	}
+
+	foo(foo&& other) noexcept
+		: _str(std::move(other._str))
+	{
+		std::cout << "constructor move foo" << std::endl;
+	}
+
+	~foo() {}
+	
+	void swap(foo& other) noexcept
+	{
+		std::cout << "swap foo" << std::endl;
+		using std::swap;
+		std::swap(_str, other._str);
+	}
+
+	foo& operator=(const foo& other)
+	{
+		std::cout << "operator copy foo" << std::endl;
+		foo(other).swap(*this);
+		return *this;
+	}
+
+	foo& operator=(foo&& other) noexcept
+	{
+		std::cout << "operator move foo" << std::endl;
+		foo(std::move(other)).swap(*this);
+		return *this;
+	}
+public:
+	std::string _str;
+};
+
+TEST(AsyncDelayTest, Test2)
+{
+	fes::async_delay<foo> sync;
+	sync.connect([](auto&& f)
+		{
+			std::cout << "<async_delay> received rvalue" << std::endl;
+		});
+	sync(0, fes::deltatime(0), foo());
+	sync.update();
+}
+
+TEST(AsyncDelayTest, Test3)
+{
+	fes::async_delay<foo> sync;
+	sync.connect([](auto&& f)
+		{
+			std::cout << "<async_delay> received lvalue" << std::endl;
+		});
+	foo f;
+	sync(0, fes::deltatime(0), f);
+	sync.update();
+}
+
