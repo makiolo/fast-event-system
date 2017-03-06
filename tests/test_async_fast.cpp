85e96243316c0cbf01c6dc45a124803212f199e0
Ricardo <makiolo@gmail.com>
2017-03-06 18:53:10 +0100

    Update test_async_fast.cpp

--- tests/test_async_fast.cpp
+++ tests/test_async_fast.cpp
@@ -116,8 +116,5 @@ TEST(AsyncFastTest, Test3)
 	sync.update();
 	ASSERT_STREQ(f._str.c_str(), "");
 	// 
-	EXPECT_CALL(f, mock_constructor()).Times(AtLeast(1));
-	EXPECT_CALL(f, mock_destructor()).Times(AtLeast(1));
 	EXPECT_CALL(f, mock_copy()).Times(0);
-	EXPECT_CALL(f, mock_move()).Times(AtLeast(1));
 }
