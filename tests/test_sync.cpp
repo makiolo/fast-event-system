aa4fada932af392b8f5313acb5c452431fc81874
Ricardo <makiolo@gmail.com>
2017-03-06 19:27:05 +0100

    Update test_sync.cpp

--- tests/test_sync.cpp
+++ tests/test_sync.cpp
@@ -2,6 +2,10 @@
 #include <gtest/gtest.h>
 #include "../sync.h"
 
+using testing::AtLeast;
+using testing::AnyNumber;
+using testing::_;
+
 class SyncTest : testing::Test { };
 
 TEST(SyncTest, Test1)
@@ -58,28 +62,41 @@ TEST(SyncTest, Test3)
 }
 
 struct foo
-{	
+{
+	MOCK_METHOD0(mock_constructor, void());
+	MOCK_METHOD0(mock_destructor, void());
+	MOCK_METHOD0(mock_copy, void());
+	MOCK_METHOD0(mock_move, void());
+	MOCK_METHOD0(mock_swap, void());
+	
 	foo()
 		: _str("bar")
 	{
+		mock_constructor();
 	}
-	
+
 	foo(const foo& other)
 		: _str(other._str)
 	{
+		mock_copy();
 		std::cout << "constructor copy foo" << std::endl;
 	}
 
 	foo(foo&& other) noexcept
 		: _str(std::move(other._str))
 	{
+		mock_move();
 		std::cout << "constructor move foo" << std::endl;
 	}
 
-	~foo() {}
+	virtual ~foo()
+	{
+		mock_destructor();
+	}
 	
 	void swap(foo& other) noexcept
 	{
+		mock_swap();
 		std::cout << "swap foo" << std::endl;
 		using std::swap;
 		std::swap(_str, other._str);
@@ -87,6 +104,7 @@ struct foo
 
 	foo& operator=(const foo& other)
 	{
+		mock_copy();
 		std::cout << "operator copy foo" << std::endl;
 		foo(other).swap(*this);
 		return *this;
@@ -94,6 +112,7 @@ struct foo
 
 	foo& operator=(foo&& other) noexcept
 	{
+		mock_move();
 		std::cout << "operator move foo" << std::endl;
 		foo(std::move(other)).swap(*this);
 		return *this;
@@ -121,6 +140,12 @@ TEST(SyncTest, Test5)
 		});
 	foo f;
 	sync(f);
+	// 
+	EXPECT_CALL(f, mock_constructor()).Times(AnyNumber());
+	EXPECT_CALL(f, mock_destructor()).Times(AnyNumber());
+	EXPECT_CALL(f, mock_copy()).Times(0);
+	EXPECT_CALL(f, mock_move()).Times(AnyNumber());
+	EXPECT_CALL(f, mock_swap()).Times(AnyNumber());
 }
 
 TEST(SyncTest, test_sync_chaining)
