c39165c0353af17539f43d543292a4ed06b2834b
Ricardo <makiolo@gmail.com>
2017-03-13 15:11:14 +0100

    Update bind.h

--- bind.h
+++ bind.h
@@ -19,53 +19,54 @@ template <typename... Args>
 class bind
 {
 public:
-	using methods = methods_t<Args...>;
+	using method = method<Args...>;
 
-	explicit bind()
-		: _registered()
-		, _conns()
+	explicit bind() : _connected(false)
 	{
 		;
 	}
 
 	template <typename T>
-	inline weak_connection<Args...> connect(T* obj, void (T::*ptr_func)(const Args&...))
+	inline void connect(T* obj, void (T::*ptr_func)(const Args&...))
 	{
-		return _connect(obj, ptr_func, make_int_sequence<sizeof...(Args)>{});
+		if(_connected)
+		{
+			throw std::runtime_error("bind already connected!");
+		}
+		_reg = method(std::bind(ptr_func, obj, placeholder_template<Is>()...));
+		_connected = true;
 	}
 	
 	template <typename METHOD>
-	inline weak_connection<Args...> connect(METHOD&& fun)
+	inline void connect(METHOD&& fun)
 	{
-		typename methods::iterator it = _registered.emplace(_registered.end(), std::forward<METHOD>(fun));
-		shared_connection<Args...> conn
-			= std::make_shared<internal_connection<Args...>>(_registered, [it](methods& registered)
-				{
-					registered.erase(it);
-				});
-		_conns.push_back(conn);
-		return weak_connection<Args...>(conn);
+		if(_connected)
+		{
+			throw std::runtime_error("bind already connected!");
+		}
+		_reg = method(std::forward<METHOD>(fun));
+		_connected = true;
 	}
 
-	inline weak_connection<Args...> connect(sync<Args...>& callback)
+	inline void connect(sync<Args...>& callback)
 	{
-		return connect([&callback](const Args&... data)
+		connect([&callback](const Args&... data)
 			{
 				callback(data...);
 			});
 	}
 
-	inline weak_connection<Args...> connect(async_fast<Args...>& queue)
+	inline void connect(async_fast<Args...>& queue)
 	{
-		return connect([&queue](Args... data)
+		connect([&queue](Args... data)
 			{
 				queue(std::move(data)...);
 			});
 	}
 
-	inline weak_connection<Args...> connect(int priority, deltatime delay, async_delay<Args...>& queue)
+	inline void connect(int priority, deltatime delay, async_delay<Args...>& queue)
 	{
-		return connect([&queue, priority, delay](Args... data)
+		connect([&queue, priority, delay](Args... data)
 			{
 				queue(priority, delay, std::move(data)...);
 			});
@@ -74,35 +75,12 @@ public:
 	template <typename ... PARMS>
 	void operator()(PARMS&&... data) const
 	{
-		auto it = _registered.begin();
-		if(it != _registered.end())
-		{
-      (*it)(std::forward<PARMS>(data)...);
-		}
+		(_reg)(std::forward<PARMS>(data)...);
 	}
 
 protected:
-	template <typename T, int... Is>
-	weak_connection<Args...> _connect(T* obj, void (T::*ptr_func)(const Args&...), int_sequence<Is...>)
-	{
-		typename methods::iterator it = _registered.emplace(
-			_registered.end(), std::bind(ptr_func, obj, placeholder_template<Is>()...));
-		shared_connection<Args...> conn
-			= std::make_shared<internal_connection<Args...> >(_registered, [it](methods& registered)
-				{
-					registered.erase(it);
-				});
-    if(_conns.size() > 0)
-    {
-		throw std::runtime_error("this bind is already connected!");
-    }
-		_conns.push_back(conn);
-		return weak_connection<Args...>(conn);
-	}
-
-protected:
-	methods _registered;
-	std::vector<shared_connection<Args...> > _conns;
+	method _reg;
+	bool _connected;
 };
 
 }
